03dd8fc27b39a3880a9e57818f3c3dfb8b1fbbd2
Niels Henrik Hagen <nhhagen@gmail.com>
2013-12-04 15:26:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Query log analysis demo
+
+[Query log analysis – using logstash, elasticsearch and kibana](http://nhhagen.wordpress.com/2013/11/28/query-log-analysis-using-logstash-elasticsearch-and-kibana/)
+
+## Running the box
+
+1. Install [Vagrant](http://www.vagrantup.com/)
+2. `vagrant up`
+3. Open http://localhost:5601/
+4. Select a dashboard from the Kibana menu.
+
+## Adding more data
+
+To add more data you need to add the index template in elasticsearch/index-temaplate.json for instructions see the elasticsearch documentation.
+
+First log into the box
+
+    vagrant ssh
+
+then you can run the logstash indexer-shipper.conf
+
+    java -jar /home/vagrant/logstash/logstash.jar agent -f '/vagrant/logstash/indexer-shipper.conf'
---
+++ manifests/default.pp
@@ -0,0 +1,126 @@
+define append_if_no_such_line($file, $line, $refreshonly = 'false') {
+   exec { "/bin/echo '$line' >> '$file'":
+      unless      => "/bin/grep -Fxqe '$line' '$file'",
+      path        => "/bin",
+      refreshonly => $refreshonly,
+   }
+}
+
+class must-have {
+  include apt
+  apt::ppa { "ppa:webupd8team/java": }
+
+  exec { 'apt-get update':
+    command => '/usr/bin/apt-get update',
+    before => Apt::Ppa["ppa:webupd8team/java"],
+  }
+
+  exec { 'apt-get update 2':
+    command => '/usr/bin/apt-get update',
+    require => [ Apt::Ppa["ppa:webupd8team/java"], Package["git-core"] ],
+  }
+
+  package { ['vim', 'curl', 'git-core', 'bash', 'wget']:
+    ensure => present,
+    require => Exec['apt-get update'],
+    before => Apt::Ppa['ppa:webupd8team/java'],
+  }
+
+  package { 'nginx':
+    ensure => installed,
+    require => Exec['apt-get update 2']
+  }
+ 
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    hasrestart => true,
+    require => Package['nginx']
+  }
+
+  file { '/etc/nginx/nginx.conf':
+    ensure => link,
+    source => '/vagrant/kibana/nginx.conf',
+    notify => Service['nginx'],
+    require => [ Package['nginx'], Exec['download_kibana'] ]
+  }
+
+  exec { 'download_kibana':
+    command => 'git clone https://github.com/elasticsearch/kibana.git',
+    cwd => '/home/vagrant',
+    user => 'vagrant',
+    path => '/usr/bin/:/bin/',
+    require => [ Package['git-core'] ],
+    logoutput => true,
+  }
+
+  file { '/home/vagrant/kibana/src/config.js':
+    ensure => link,
+    source => '/vagrant/kibana/config.js',
+    notify => Service['nginx'],
+    require => [ Package['nginx'], Exec['download_kibana'] ]
+  }
+
+  package { 'oracle-java7-installer':
+    ensure => present,
+    require => Exec['apt-get update 2'],
+  }
+
+  exec { "accept_license":
+    command => "echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections && echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections",
+    cwd => "/home/vagrant",
+    user => "vagrant",
+    path => "/usr/bin/:/bin/",
+    require => Package["curl"],
+    before => Package["oracle-java7-installer"],
+    logoutput => true,
+  }
+
+  file { '/home/vagrant/logstash':
+    ensure => directory,
+    owner => 'vagrant',
+    group => 'vagrant',
+    before => Exec['download_logstash']
+  }
+
+  file { '/home/vagrant/logstash/logstash.jar':
+   ensure => link,
+   owner => 'vagrant',
+   group => 'vagrant',
+   target => '/home/vagrant/logstash/logstash-1.2.2-flatjar.jar',
+   require => Exec['download_logstash']
+  }
+
+  exec { 'download_logstash':
+    command => 'wget -q https://download.elasticsearch.org/logstash/logstash/logstash-1.2.2-flatjar.jar',
+    cwd => '/home/vagrant/logstash',
+    user => 'vagrant',
+    path => '/usr/bin/:/bin/',
+    require => [ Package['wget'] ],
+    logoutput => true,
+  }
+
+  class { 'elasticsearch':
+    package_url => 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.0.Beta2.deb',
+    config => {
+      'cluster.name' => 'vagrant_elasticsearch',
+      'node.name' => $::ipaddress,
+      'index' => {
+        'number_of_replicas' => '0',
+        'number_of_shards' => '1',
+      },
+      'network' => {
+        'host' => $::ipaddress,
+      },
+      'path' => {
+        'conf' => '/elasticsearch_home/config',
+        'data' => '/vagrant/sample_index',
+        'work' => '/elasticsearch_home/work',
+        'logs' => '/elasticsearch_home/logs',
+        'plugins' => '/elasticsearch_home/plugins'
+      }
+    }
+  }
+}
+
+include must-have
